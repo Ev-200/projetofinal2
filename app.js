d51a23701569a393e104632f2fefee4b13ec9a77
Ev-200 <1269@iai.pt>
2026-02-03 11:03:40 +0000

    .

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+let coverDisplay = false
+
 let lamp_1;
-
-
+let cover_1;
+let xLamp;
+let yLamp;
+let diametro = 90;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     angleMode(DEGREES);
 
-    lamp_1 = new Lamp(90, 220)
+    lamp_1 = new Lamp(90, 220, 450)
+    cover_1 = new LampCover(15, 100)
+
 
 }
 
@@ -25,24 +31,55 @@ function draw() {
 
     lamp_1.displayLamp();
 
-    quad(20, 30, 80, 30, 100, 90, 0, 90);
-
+    if (coverDisplay == true) {
+        cover_1.displayCover();
+    }
 }
 
-class Lamp {
+class LampCover {
     constructor(x, y) {
-        this.x = x;
+        this.x = x
         this.y = y
+    }
+
+    displayCover() {
+        push()
+        fill(49, 51, 50)
+        noStroke()
+        scale(1.5)
+        translate(this.x, this.y)
+        quad(20, 40, 80, 40, 100, 90, 0, 90);
+        ellipse(50, 50, 70, 70)
+        pop()
+    }
+}
+
+
+class Lamp {
+    constructor(xLamp, yLamp, height) {
+        this.xLamp = xLamp;
+        this.yLamp = yLamp;
+        this.height = height
     }
 
     displayLamp() {
         fill(49, 51, 50)
-        rect(this.x, this.y, 15, 450, 0, 0, 5, 5)
+        rect(this.xLamp, this.yLamp, 15, this.height, 0, 0, 5, 5)
         fill(220, 240, 200)
-        ellipse(this.x + 5, this.y, 90, 90)
+        ellipse(this.xLamp + 5, this.yLamp, diametro, diametro)
 
 
     }
 
 }
-mousePressed()
+
+
+function mousePressed() {
+    if (mouseX > xLamp - diametro / 2 && mouseX < xLamp + diametro / 2) {
+        if (mouseY > yLamp - diametro / 2 && mouseY < yLamp + diametro / 2) {
+
+            console.log("test")
+            coverDisplay = true
+        }
+    }
+}
